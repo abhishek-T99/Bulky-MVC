5f6c5e786c30d0c3152ba1b1eb4e0239aa5bd0a9
abhishek-T99 <timilsina.abhishek55@gmail.com>
2025-03-09 22:08:58 +0545

    feat: Added database connection string in appsettings.json

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -7,7 +7,7 @@ public class Category
     [Key]
     public int Id { get; set; }
     [Required, MaxLength(250)]
-    public string Name { get; set; }
+    public required string Name { get; set; }
     [Required]
     public int DisplayOrder { get; set; }
 }
